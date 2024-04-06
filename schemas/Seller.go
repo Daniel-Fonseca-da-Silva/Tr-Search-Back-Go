51ff6d451e1bb617fe5814fd8f36710db1ac6ca0
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2024-04-06 11:09:31 -0300

    feat: add seller schema package

---
+++ schemas/Seller.go
@@ -0,0 +1,17 @@
+package schemas
+
+import "gorm.io/gorm"
+
+type Seller struct {
+	gorm.Model
+	Site        string `gorm:"type:varchar(7);"`
+	Document    string `gorm:"type:varchar(7);not null"`
+	About       string `gorm:"type:varchar(7);not null"`
+	Status      bool   `gorm:"type:boolean;not null"`
+	Username    string `gorm:"type:varchar(50);not null"`
+	Password    string `gorm:"type:varchar(20);not null"`
+	DisplayName string `gorm:"type:varchar(20);not null"`
+	PhotoID     uint
+	Favorite    Favorite
+	AddressID   uint
+}
